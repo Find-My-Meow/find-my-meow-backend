9a8394154cad1fe4c5dd6f59ff36a26adde8141e
Jindaporn Sookying <jindaporn.sy@gmail.com>
2025-02-10 10:50:42 +0700

    add search by location

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from database import lifespan
 from routes.posts import post_router
+from routes.search import search_router
 
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(post_router, prefix="/api/v1/posts", tags=["Posts"])
+app.include_router(search_router, prefix="/api/v1/search", tags=["Search"])
 
 @app.get("/")
 async def root():
